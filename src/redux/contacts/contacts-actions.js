7c084bf9daacccd8550230c1190eaa4441b5b34e
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-11-14 22:51:56 +0200

    store в редакс тулкит

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,23 +1,35 @@
 import types from "./contacts-types";
 import shortid from "shortid";
+import { createAction } from "@reduxjs/toolkit";
 
-const addContact = ({ name, number }) => ({
-  type: types.ADD,
+const addContact = createAction("contact/Add", ({ name, number }) => ({
   payload: {
     id: shortid.generate(),
     name,
     number,
   },
-});
+}));
 
-const deleteContact = (contactId) => ({
-  type: types.DELETE,
-  payload: contactId,
-});
+// const addContact = ({ name, number }) => ({
+//   type: types.ADD,
+//   payload: {
+//     id: shortid.generate(),
+//     name,
+//     number,
+//   },
+// });
 
-const changeFilter = (value) => ({
-  type: types.CHANGE_FILTER,
-  payload: value,
-});
+const deleteContact = createAction("contacts/Delete");
+// const deleteContact = (contactId) => ({
+//   type: types.DELETE,
+//   payload: contactId,
+// });
+
+const changeFilter = createAction("contacts/Filter");
+
+// const changeFilter = (value) => ({
+//   type: types.CHANGE_FILTER,
+//   payload: value,
+// });
 
 export default { addContact, deleteContact, changeFilter };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
-import { combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 import contactsReducer from "./contacts/contacts-reducer";
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
+const middleware = [...getDefaultMiddleware(), logger];
+
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === "development",
 });
-
-const store = createStore(rootReducer, composeWithDevTools());
-
 export default store;
